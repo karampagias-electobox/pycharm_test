bcdaf8cd17c179a479d1bfbe997dedd900a010b4
karampagias-electobox <karampagias@electobox.com>
2024-05-09 12:28:07 +0300

    v3

--- test_app/models.py
+++ test_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class TestModel(models.Model):
-    name = models.CharField(max_length=100, verbose_name="something", blank=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
