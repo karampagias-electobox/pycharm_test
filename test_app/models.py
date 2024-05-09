25f135d46734bf790054da689773afe11d296f6f
your name here <your electobox email here>
2024-05-09 12:23:49 +0300

    v2

--- test_app/models.py
+++ test_app/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class TestModel(models.Model):
+    name = models.CharField(max_length=100, verbose_name="something", blank=True)
 
-    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class TestModel2(models.Model):
+    name = models.CharField(max_length=100, verbose_name="something", blank=True, null=True)
 
     def __str__(self):
         return self.name
